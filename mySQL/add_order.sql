24cad80054170b2365d313fb9d55ee1fee9ba974
Abdelrahmannnn <45874233+Abdelrahmannnn@users.noreply.github.com>
2021-06-13 00:45:43 +0200

    Delete mySQL directory

--- mySQL/add_order.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE add_order (ISBN char(15), quantity int unsigned)
-BEGIN
-	insert into book_store.order (ISBN, date, num_of_books) values (ISBN, NOW(), quantity);
-END //
-DELIMITER ;
